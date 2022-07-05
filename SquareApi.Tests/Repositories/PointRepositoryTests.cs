5455ada7e976d78513fce96a554f2d64bc1cd9e9
Sumit Kumar <sumit.kumar13@nagarro.com>
2022-07-05 11:52:44 +0530

    project restructing & repositories test

---
+++ SquareApi.Tests/Repositories/PointRepositoryTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SquareApi.Data;
+using SquareApi.Data.Contracts;
+using SquareApi.Models;
+
+namespace SquareApi.Tests.Repositories;
+
+[TestClass]
+public class PointRepositoryTests
+{
+    private readonly IUnitofWork _uow;
+    private List<Point> _points => new List<Point>
+    {
+        new Point { X = 0, Y = 0 },
+        new Point { X = 0, Y = 1 },
+        new Point { X = 1, Y = 0 },
+        new Point { X = 1, Y = 1 }
+    };
+
+    public PointRepositoryTests()
+    {
+        var builder = new DbContextOptionsBuilder<SquareApiDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString());
+        var context = new SquareApiDbContext(builder.Options);
+        _uow = new UnitOfWork(context);
+    }
+
+    [TestMethod]
+    public async Task AddPointTest()
+    {
+        await _uow.Points.AddRangeAsync(_points);
+        await _uow.SaveChangesAsync();
+
+        int count = (await _uow.Points.GetAllAsync()).Count();
+
+        Assert.AreEqual(4, count);
+    }
+
+    [TestMethod]
+    public async Task DeletePointTest()
+    {
+        await _uow.Points.AddRangeAsync(_points);
+        await _uow.SaveChangesAsync();
+
+        await _uow.Points.DeleteAsync(2);
+        await _uow.SaveChangesAsync();
+
+        int count = (await _uow.Points.GetAllAsync()).Count();
+
+        Assert.AreEqual(3, count);
+    }
+}
